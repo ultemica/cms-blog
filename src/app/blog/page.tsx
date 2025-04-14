dd7f2dc4e1579b27e932a3ea402b1706466cd16d
ultemica <ultemica@proton.me>
2025-04-14 12:24:00 +0000

    feat(pagination): add pagination component and integrate with blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,13 @@
+import { Pages } from '@/src/components/Pagination'
+import { Client } from '@/src/models/schema'
+
 export default async function Page() {
+  const response = await Client.get('/blogs')
   return (
     <>
       {/* <BlogList blogs={response.data} /> */}
       {/* @ts-ignore */}
+      <Pages />
       {/* <Pagination totalCount={response.meta.pagination.total} /> */}
     </>
   )

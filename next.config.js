4c7e4673632ae0ecd45b2f2ed649c693c3cafa5d
ultemica <ultemica@proton.me>
2025-04-14 17:50:56 +0000

    refactor(build): update build output paths and comment out dev setup

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
+  // distDir: 'build',
   // output: 'export',
   images: {
     remotePatterns: [new URL('https://cdn.bsky.app/**')]
   }
 }
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform()
-}
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform()
+// }
 
 module.exports = nextConfig

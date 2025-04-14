02369de1cf4fddb00df35083b2c919e4216b9890
ultemica <ultemica@proton.me>
2025-04-14 12:02:06 +0000

    refactor(app): simplify terminal message rendering logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,25 @@
 'use client'
 import { format } from 'date-fns'
 import type React from 'react'
-import { useEffect, useState } from 'react'
 
 export const ReactTerminal: React.FC = () => {
-  const [message, setMessage] = useState('root@ubuntu:~# ssh root@localhost -i ~./.ssh/id_ed25519')
-  const [blink, setBlink] = useState(true)
-
-  useEffect(() => {
-    const blinkInterval = setInterval(() => setBlink((prev) => !prev), 500)
-    const timer = setTimeout(() => {
-      const lines = [
-        'Welcome to Under+Ground 22.04.5 LTS (GNU/Linux 6.8.0-52-generic x86_64)',
-        '',
-        ' * Documentation:  https://help.ubuntu.com',
-        ' * Management:     https://landscape.canonical.com',
-        ' * Support:        https://ubuntu.com/pro',
-        '',
-        "New release '24.04.2 LTS' available.",
-        "Run 'do-release-upgrade' to upgrade to it.",
-        '',
-        `Last login: ${format(new Date(), 'EEE MMM dd HH:mm:ss yyyy')} from 172.18.0.7`,
-        'root@ubuntu:~#'
-      ]
-      setMessage(lines.join('\n'))
-    }, 1500)
-    return () => {
-      clearTimeout(timer)
-      clearInterval(blinkInterval)
-    }
-  }, [])
-
   return (
     <div className='font-mono p-4 bg-gray-900 text-gray-200 overflow-auto'>
-      <pre>{message.startsWith('Connecting') ? (blink ? 'Connecting...' : 'Connecting') : message}</pre>
+      <pre>
+        {[
+          'Welcome to Under+Ground 22.04.5 LTS (GNU/Linux 6.8.0-52-generic x86_64)',
+          '',
+          ' * Documentation:  https://help.ubuntu.com',
+          ' * Management:     https://landscape.canonical.com',
+          ' * Support:        https://ubuntu.com/pro',
+          '',
+          "New release '24.04.2 LTS' available.",
+          "Run 'do-release-upgrade' to upgrade to it.",
+          '',
+          `Last login: ${format(new Date(), 'EEE MMM dd HH:mm:ss yyyy')} from 172.18.0.7`,
+          'root@ubuntu:~#'
+        ].join('\n')}
+      </pre>
     </div>
   )
 }

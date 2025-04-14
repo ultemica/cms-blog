459af14023209b632d3014d7ff8709b50663ed7b
ultemica <ultemica@proton.me>
2025-04-14 11:47:26 +0000

    feat(about): add animated rendering of markdown content on page load

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,26 @@
+'use client'
 import { Client } from '@/src/models/schema'
+import { useEffect, useState } from 'react'
 import markdownToHtml from 'zenn-markdown-html'
 
-export default async function Page() {
-  const res = await Client.get('/about')
-  // @ts-ignore
-  const html = markdownToHtml(res.data.description)
+export default function Page() {
+  const [html, setHtml] = useState('')
+  useEffect(() => {
+    ;(async () => {
+      const res = await Client.get('/about')
+      const fullHtml = markdownToHtml(res.data.description)
+      let i = 0
+      const intervalId = setInterval(() => {
+        i++
+        setHtml(fullHtml.slice(0, i))
+        if (i >= fullHtml.length) clearInterval(intervalId)
+      }, 10)
+    })()
+  }, [])
   return (
     <>
       <div className='font-mono'>
-        <div className='text-gray-900 dark:text-gray-300 py-8 prose font-grotesk'>
+        <div className='text-gray-900 dark:text-gray-300'>
           {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
           <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>

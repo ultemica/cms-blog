6e755da0fc9a78bee049492a53fb02673e43f028
ultemica <ultemica@proton.me>
2025-04-14 11:45:12 +0000

    refactor(about): simplify font usage and clean up inner HTML rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,5 @@
 import { Client } from '@/src/models/schema'
-import { Space_Grotesk } from 'next/font/google'
 import markdownToHtml from 'zenn-markdown-html'
-
-const grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '500', '600', '700']
-})
 
 export default async function Page() {
   const res = await Client.get('/about')
@@ -14,10 +7,10 @@ export default async function Page() {
   const html = markdownToHtml(res.data.description)
   return (
     <>
-      <div className={grotesk.className}>
+      <div className='font-mono'>
         <div className='text-gray-900 dark:text-gray-300 py-8 prose font-grotesk'>
           {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
-          <div className='' dangerouslySetInnerHTML={{ __html: html }} />
+          <div dangerouslySetInnerHTML={{ __html: html }} />
         </div>
       </div>
     </>

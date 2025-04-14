db1e2a773e80bdfb5ff28fc3b3ed9c25e494a070
ultemica <ultemica@proton.me>
2025-04-14 11:36:20 +0000

    refactor(app): remove react-xtermjs and related dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-'use client'
 import type React from 'react'
-import { useXTerm } from 'react-xtermjs'
 
 export const ReactTerminal: React.FC = () => {
-  const { instance, ref } = useXTerm()
-  instance?.writeln('Hello from react-xtermjs!')
-  instance?.onData((data) => instance?.write(data))
-
-  return <div ref={ref} style={{ width: '100%', height: '100%' }} />
+  return <></>
 }
 
 export default async function Page() {

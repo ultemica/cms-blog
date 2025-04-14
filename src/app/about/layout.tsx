321572d305558fb132954615057a93e2b128eeaa
ultemica <ultemica@proton.me>
2025-04-14 11:48:59 +0000

    refactor(about): adjust layout styles and remove unused content

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -14,12 +14,12 @@ export default async function RootLayout({
   const response: About = await Client.get('/about')
   return (
     <>
-      <div className='divide-y divide-gray-200 dark:divide-gray-700'>
-        <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
+      <div className='divide-y divide-gray-200 dark:divide-gray-700 font-mono'>
+        {/* <div className='space-y-2 pt-6 pb-8 md:space-y-5'>
           <h1 className='text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100'>
             About
           </h1>
-        </div>
+        </div> */}
         <div className='items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0'>
           <div className='flex flex-col items-center space-x-2 pt-8'>
             <Image
@@ -30,7 +30,6 @@ export default async function RootLayout({
               className='h-48 w-48 rounded-full'
             />
             <h3 className='pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight'>{response.data.name}</h3>
-            <div className='text-gray-500 dark:text-gray-400'>{'Rakunan Senior & Junior High School'}</div>
             <div className='text-gray-500 dark:text-gray-400'>{response.data.organization}</div>
             <div className='flex space-x-3 pt-6'>
               <Link href='https://bsky.app/profile/tkgstrator.work'>

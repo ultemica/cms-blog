04248a37a964b78d8b003ecd73c5f52005a22103
ultemica <ultemica@proton.me>
2025-04-15 09:08:07 +0000

    feat(styles): add background color to blog page and bold headings globally

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -45,7 +45,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
           <div className='relative'>
             <div className='flex space-between'>
               <section className='xl:w-[calc(100%-330px)] w-full'>
-                <div className='border-[1px] py-[40px] text-[16px]'>
+                <div className='border-[1px] py-[40px] text-[16px] bg-white'>
                   <div className='m-w-4xl px-[40px]'>
                     <div
                       className='znc'

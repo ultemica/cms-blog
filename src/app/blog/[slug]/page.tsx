b291984d6d8b2c7828e5139e56562b2dbbf75549
ultemica <ultemica@proton.me>
2025-04-15 08:39:12 +0000

    refactor(blog): update blog header layout and styling

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -30,10 +30,16 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
 
   return (
     <div>
-      <div className='pb-2 mb-2 border-b'>
-        <h1 className='text-3xl font-bold mb-1'>{response.data.title}</h1>
-        <p className='text-gray-500 dark:text-gray-400'>{response.data.publishedAt.toDateString()}</p>
-      </div>
+      <header className='pt-[3.4rem] pb-[3.2rem] px-0 text-center'>
+        <div className='m-w-6xl mx-auto px-[40px]'>
+          <div className='relative'>
+            <h1 className='inline-block max-w-[780px] my-[1.2rem] text-[33px] text-left'>
+              <span className='text-[0.84em]'>{response.data.title}</span>
+            </h1>
+            <p className='text-gray-500 dark:text-gray-400'>{response.data.publishedAt.toDateString()}</p>
+          </div>
+        </div>
+      </header>
       {/* biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation> */}
       <div className='znc' dangerouslySetInnerHTML={{ __html: html }} />
     </div>

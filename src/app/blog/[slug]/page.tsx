41ec9b5cbcf2a68a54aced935b082dc798a53a1a
ultemica <ultemica@proton.me>
2025-05-07 15:49:29 +0000

    feat(blog): add author info and table of contents to blog page layout

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,6 +1,10 @@
+import { TableOfContents } from '@/components/ToC'
 import type { BlogDatum, BlogList } from '@/models/blog.dto'
 import { Client } from '@/models/schema'
-import {} from 'react-icons/bs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { BsGithub, BsTwitterX } from 'react-icons/bs'
+import { FaBluesky } from 'react-icons/fa6'
 import markdownToHtml from 'zenn-markdown-html'
 
 export const revalidate = 10
@@ -17,11 +21,7 @@ export async function generateStaticParams() {
   }))
 }
 
-export default async function Page({
-  params
-}: {
-  params: Promise<{ slug: string }>
-}) {
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
   const response = await Client.get('/blogs/:id', {
     params: {
@@ -31,11 +31,10 @@ export default async function Page({
       populate: 'categories'
     }
   })
-  const markdown = response.data.content
-  const html = markdownToHtml(markdown)
+  const html = markdownToHtml(response.data.content)
 
   return (
-    <div className='w-full'>
+    <div>
       <header className='pt-[0.0rem] pb-[3.2rem] px-0 text-center'>
         <div className='m-w-6xl mx-auto px-[40px]'>
           <div className='relative'>
@@ -61,16 +60,11 @@ export default async function Page({
       </header>
       <div className='m-w-6xl mx-auto px-0 sm:px-[40px]'>
         <div className='block'>
-          <div className='relative flex'>
+          <div className='relative'>
             <div className='flex justify-between'>
-              <section className='w-full md:w-[calc(100%-330px)]'>
+              <section className='lg:w-[calc(100%-330px)] w-full'>
                 <div className='py-[40px] bg-white dark:bg-gray-900 rounded-md'>
-                  <div
-                    className='m-w-4xl px-[14px] sm:px-[40px]'
-                    style={{
-                      fontSize: '1.15em'
-                    }}
-                  >
+                  <div className='m-w-4xl px-[25px] md:px-[40px] text-lg'>
                     <div
                       className='znc'
                       // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
@@ -79,6 +73,35 @@ export default async function Page({
                   </div>
                 </div>
               </section>
+              <aside className='hidden lg:block lg:w-[300px]'>
+                <div className='p-[20px] bg-white dark:bg-gray-900 rounded-md'>
+                  <div className='flex items-center justify-between'>
+                    <Image src='/author.webp' width={60} height={60} alt='tkgstrator' className='rounded-full' />
+                    <div className='w-[calc(100%-70px)]'>
+                      <span className='mt-[3px] text-[1.2rem] font-bold'>tkgstrator</span>
+                      <div className='flex gap-[10px] items-center mt-[7px]'>
+                        <Link href='https://bsky.app/profile/tkgstrator.work'>
+                          <FaBluesky />
+                        </Link>
+                        <Link href='https://x.com/tkgling'>
+                          <BsTwitterX />
+                        </Link>
+                        <Link href='https://github.com/tkgstrator'>
+                          <BsGithub />
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                  <span className='block h-[1.5rem]' />
+                  <p>美少女JKエンジニア</p>
+                </div>
+                <span className='block h-[1.5rem]' />
+                <div className='sticky top-[1.5rem]'>
+                  <div>
+                    <TableOfContents content={response.data.content} />
+                  </div>
+                </div>
+              </aside>
             </div>
           </div>
         </div>
